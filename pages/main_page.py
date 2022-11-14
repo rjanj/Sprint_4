e62bd0236aef7a8cd6179877de1de18e1e3caf2a
Roman Dzhandzhgava <r.janjgava@yandex.ru>
2022-11-15 01:35:52 +0400

    upd

--- pages/main_page.py
+++ pages/main_page.py
@@ -37,63 +37,6 @@ class MainPageScooter:
     def scroll_into_questions(self):
         main_questions = self.driver.find_element(*self.scroll_in_to_questions)
         self.driver.execute_script("arguments[0].scrollIntoView();", main_questions)
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "Home_FAQ__3uVm4")))
-
-    @allure.step('Открываем первый вопрос и получаем на него ответ')
-    def click_question_one_and_take_answer(self):
-        self.driver.find_element(*self.question_one_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-0')))
-        question_one_answer = self.driver.find_element(*self.question_one_answer).text
-        return question_one_answer
-
-    @allure.step('Открываем второй вопрос и получаем на него ответ')
-    def click_question_two_and_take_answer(self):
-        self.driver.find_element(*self.question_two_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-1')))
-        question_two_answer = self.driver.find_element(*self.question_two_answer).text
-        return question_two_answer
-
-    @allure.step('Открываем третий вопрос и получаем на него ответ')
-    def click_question_three_and_take_answer(self):
-        self.driver.find_element(*self.question_three_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-2')))
-        question_three_answer = self.driver.find_element(*self.question_three_answer).text
-        return question_three_answer
-
-    @allure.step('Открываем четвертый вопрос и получаем на него ответ')
-    def click_question_four_and_take_answer(self):
-        self.driver.find_element(*self.question_four_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-3')))
-        question_four_answer = self.driver.find_element(*self.question_four_answer).text
-        return question_four_answer
-
-    @allure.step('Открываем пятый вопрос и получаем на него ответ')
-    def click_question_five_and_take_answer(self):
-        self.driver.find_element(*self.question_five_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-4')))
-        question_five_answer = self.driver.find_element(*self.question_five_answer).text
-        return question_five_answer
-
-    @allure.step('Открываем шестой вопрос и получаем на него ответ')
-    def click_question_six_and_take_answer(self):
-        self.driver.find_element(*self.question_six_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-5')))
-        question_six_answer = self.driver.find_element(*self.question_six_answer).text
-        return question_six_answer
-
-    @allure.step('Открываем седьмой вопрос и получаем на него ответ')
-    def click_question_seven_and_take_answer(self):
-        self.driver.find_element(*self.question_seven_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-6')))
-        question_seven_answer = self.driver.find_element(*self.question_seven_answer).text
-        return question_seven_answer
-
-    @allure.step('Открываем восьмой вопрос и получаем на него ответ')
-    def click_question_eight_and_take_answer(self):
-        self.driver.find_element(*self.question_eight_text).click()
-        WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.ID, 'accordion__panel-7')))
-        question_eight_answer = self.driver.find_element(*self.question_eight_answer).text
-        return question_eight_answer
 
     def main_cookies_accept(self):
         self.driver.find_element(*self.cookies_accept).click()
@@ -102,3 +45,10 @@ class MainPageScooter:
         self.navigate()
         self.main_cookies_accept()
         self.scroll_into_questions()
+
+    def click_question_and_take_answer(self, click, question):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "Home_FAQ__3uVm4")))
+        self.driver.find_element(*click).click()
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(question))
+        question_answer = self.driver.find_element(*question).text
+        return question_answer
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,4 +1,5 @@
 import allure
+
 from pages.main_page import MainPageScooter
 
 
@@ -9,7 +10,10 @@ class TestQuestions:
     def test_first_question(self, driver):
         question_one = MainPageScooter(driver)
         question_one.navigate_cookie_scroll()
-        question_one_text = question_one.click_question_one_and_take_answer()
+        question_one_text = question_one.click_question_and_take_answer(
+            click=question_one.question_one_text,
+            question=question_one.question_one_answer
+        )
         assert question_one_text == 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.'
 
     @allure.description(
@@ -17,7 +21,10 @@ class TestQuestions:
     def test_second_question(self, driver):
         question_two = MainPageScooter(driver)
         question_two.navigate_cookie_scroll()
-        question_two_text = question_two.click_question_two_and_take_answer()
+        question_two_text = question_two.click_question_and_take_answer(
+            click=question_two.question_two_text,
+            question=question_two.question_two_answer
+        )
         assert question_two_text == 'Пока что у нас так: один заказ — один самокат. Если хотите покататься с ' \
                                     'друзьями, можете просто сделать несколько заказов — один за другим.'
 
@@ -26,7 +33,10 @@ class TestQuestions:
     def test_third_question(self, driver):
         question_three = MainPageScooter(driver)
         question_three.navigate_cookie_scroll()
-        question_three_text = question_three.click_question_three_and_take_answer()
+        question_three_text = question_three.click_question_and_take_answer(
+            click=question_three.question_three_text,
+            question=question_three.question_three_answer
+        )
         assert question_three_text == 'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение ' \
                                       'дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ ' \
                                       'курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 ' \
@@ -37,7 +47,10 @@ class TestQuestions:
     def test_fourth_question(self, driver):
         question_four = MainPageScooter(driver)
         question_four.navigate_cookie_scroll()
-        question_four_text = question_four.click_question_four_and_take_answer()
+        question_four_text = question_four.click_question_and_take_answer(
+            click=question_four.question_four_text,
+            question=question_four.question_four_answer
+        )
         assert question_four_text == 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.'
 
     @allure.description(
@@ -45,7 +58,10 @@ class TestQuestions:
     def test_fifth_question(self, driver):
         question_five = MainPageScooter(driver)
         question_five.navigate_cookie_scroll()
-        question_five_text = question_five.click_question_five_and_take_answer()
+        question_five_text = question_five.click_question_and_take_answer(
+            click=question_five.question_five_text,
+            question=question_five.question_five_answer
+        )
         assert question_five_text == 'Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по ' \
                                      'красивому номеру 1010.'
 
@@ -54,7 +70,10 @@ class TestQuestions:
     def test_sixth_question(self, driver):
         question_six = MainPageScooter(driver)
         question_six.navigate_cookie_scroll()
-        question_six_text = question_six.click_question_six_and_take_answer()
+        question_six_text = question_six.click_question_and_take_answer(
+            click=question_six.question_six_text,
+            question=question_six.question_six_answer
+        )
         assert question_six_text == 'Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже ' \
                                     'если будете кататься без передышек и во сне. Зарядка не понадобится.'
 
@@ -63,7 +82,10 @@ class TestQuestions:
     def test_seventh_question(self, driver):
         question_seven = MainPageScooter(driver)
         question_seven.navigate_cookie_scroll()
-        question_seven_text = question_seven.click_question_seven_and_take_answer()
+        question_seven_text = question_seven.click_question_and_take_answer(
+            click=question_seven.question_seven_text,
+            question=question_seven.question_seven_answer
+        )
         assert question_seven_text == "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не " \
                                       "попросим. Все же свои."
 
@@ -72,5 +94,8 @@ class TestQuestions:
     def test_eight_question(self, driver):
         question_eight = MainPageScooter(driver)
         question_eight.navigate_cookie_scroll()
-        question_eight_text = question_eight.click_question_eight_and_take_answer()
+        question_eight_text = question_eight.click_question_and_take_answer(
+            click=question_eight.question_eight_text,
+            question=question_eight.question_eight_answer
+        )
         assert question_eight_text == 'Да, обязательно. Всем самокатов! И Москве, и Московской области.'
